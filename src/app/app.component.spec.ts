62f96c065e339973cb24c9d14c4697c3385d3cb2
Alexander Gerbig <agerbig@systhemis.de>
2021-02-18 15:36:08 +0100

    Angular jest playground app

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { TestModel } from './app.model';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Angular-Jest-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Angular-Jest-Playground');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'Angular-Jest-Playground app is running!'
+    );
+  });
+
+  it('should use model', () => {
+    const testModel: TestModel = {
+      id: '1',
+      name: 'Alex',
+      country: 'Deutschland',
+    };
+    console.log(testModel);
+  });
+});
---
+++ src/app/app.model.ts
@@ -0,0 +1,6 @@
+export interface TestModel {
+  id: string;
+  name: string;
+  country: string;
+  language: string;
+}
